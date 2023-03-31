c9cc08425c9b2562ec83eca38e49ac90e6bb4872
hubertway <32203359+hubertway@users.noreply.github.com>
2023-03-31 17:40:46 +0800

    应该用并查集。dfs数据多的时候会出现段错误，推断是OJ的内存限制。

---
+++ 合根植物.cpp
@@ -0,0 +1,57 @@
+//并查集
+#include <iostream>
+using namespace std;
+int root[1000001] = { 0 };
+int  findroot(int x) {
+    return x == root[x] ? x : root[x] = findroot(root[x]);
+}
+int main() {
+    int m, n, k;
+    cin >> m >> n >> k;
+    //初始化root数组，让每个点都以自己为根节点
+    int sum = m * n;
+    for (int i = 0; i < sum; i++) root[i] = i;
+
+    for (int i = 0; i < k; i++) {
+        int u, v;
+        cin >> u >> v;
+        if (findroot(u) != findroot(v)) {
+            root[findroot(u)] = findroot(v);
+            sum--;
+        }
+    }
+    cout << sum;
+    return 0;
+}
+
+
+
+//以下为dfs版本
+#include <iostream>
+using namespace std;
+int g[1001][1001] = { 0 };
+int visited[1000001] = { false };
+int m, n, k;
+void dfs(int v) {
+    visited[v] = true;
+    for (int i = 1; i <= m * n ; i++) 
+        if (g[v][i] == 1 && !visited[i])  dfs(i);
+}
+int main() {
+    cin >> m >> n >> k;
+    for (int i = 0; i < k; i++) {
+        int u, v;
+        cin >> u >> v;
+        g[u][v] = 1;
+        g[v][u] = 1;
+    }
+    int ans = 0;
+    for (int ver = 1; ver <= m * n; ver++) {
+        if (!visited[ver]) {
+            dfs(ver);
+            ans++;
+        }
+    }
+    cout << ans;;
+    return 0;
+}
